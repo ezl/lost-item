99af0cdb0652a397d1636f491b5d3986eb501230
ezl <ericzliu@gmail.com>
2016-12-13 19:19:29 -0600

    Nav component

--- components/App.jsx
+++ components/App.jsx
@@ -1,12 +1,33 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
+
 const propTypes = {
   children: PropTypes.element.isRequired,
   routes: PropTypes.array.isRequired,
 };
 
 function App({ children, routes }) {
+  class Links extends React.Component {
+    render() {
+      return(
+        <ul>
+          <li><a href="https://wwww.google.com">Google</a></li>
+          <li><a href="https://wwww.yahoo.com">Yahoo</a></li>
+        </ul>
+      )
+    }
+  }
+  class Nav extends React.Component {
+    render() {
+      return (
+        <nav>
+          {generateMapMenu()}
+          <Links />
+        </nav>
+      )
+    }
+  }
   function generateMapMenu() {
     let path = '';
 
@@ -29,15 +50,11 @@ function App({ children, routes }) {
     );
   }
 
-  const repoLink = 'https://github.com/rafrex/spa-github-pages';
-
   return (
     <div>
       <h1>Lost Items</h1>
       <h2>Never Lose Your Stuff Again</h2>
-      <nav>
-        {generateMapMenu()}
-      </nav>
+      <Nav />
       {children}
     </div>
   );
