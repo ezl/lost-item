38a0c5514219ccc5e38598683ace8843ff6514bc
ezl <ericzliu@gmail.com>
2016-12-14 14:57:32 -0600

    bootstrap form

--- components/HowItWorks.jsx
+++ components/HowItWorks.jsx
@@ -10,8 +10,14 @@ function HowItWorks() {
     <div>
       <h1>How It Works</h1>
       <p>
-        If you lose stuff and it has your link on it, whoever finds it can type that link in and use it to contact you.
+        If you lose stuff and it has your link on it, whoever finds it go to that link and use it to contact you.
       </p>
+      <ol>
+        <li>Put your link, like <a href="/eric">www.lost-item.com/eric</a> on something you belong.</li>
+        <li>Lose that item.</li>
+        <li>When a kind and benevolent stranger finds that item, he/she goes to your link, the contacts you.</li>
+        <li>Coordinate with stranger to recover your lost item.</li>
+      </ol>
     </div>
   );
 }
--- components/Signup.jsx
+++ components/Signup.jsx
@@ -9,25 +9,32 @@ class SignUpForm extends React.Component {
   render() {
     return (
       <form>
-        <div>
+        <div className="form-group">
           <label>What is your name?</label>
-          <input type="text" name="name" />
-          <p>If someone you know finds something that belongs to you, they can give it to you directly</p>
+          <input className="form-control" type="text" name="name" />
+          <small className="form-text text-muted">If someone you know finds something that belongs to you, they can give it to you directly</small>
         </div>
-        <div>
+        <div className="form-group">
           <label>What is your email address?</label>
-          <input type="email" name="email" />
-          <p>So if someone you do not know finds something of yours, they'll let us know, and we can contact you to let you know.</p>
+          <input className="form-control" type="email" name="email" />
+          <small className="form-text text-muted">So if someone you do not know finds something of yours, they'll let us know, and we can contact you to let you know.</small>
         </div>
-        <div>Privacy</div>
-        <div>
+
+        <fieldset className="form-group">
           <p>If someone finds something that belongs to you and goes to your link, do you want us to display the email address to them to contact you directly? Or hide it using a form to contact you through us?</p>
-          <p><input type="radio" name="contact" value="form" /> Hide my email and contact me through Lost-Item.Com</p>
-          <p><input type="radio" name="contact" value="direct" /> Have them email me directly.</p>
-        </div>
-        <div>
-          <input disabled type="submit" value="Get Your Own Lost Item Link!" />
-        </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input type="radio" className="form-check-input" name="contact" value="direct" /> Have them email me directly.
+            </label>
+          </div>
+          <div className="form-check">
+            <label className="form-check-label">
+              <input type="radio" className="form-check-input" name="contact" value="form" /> Hide my email and them contact me through Lost-Item.Com
+            </label>
+          </div>
+        </fieldset>
+
+        <button type="submit" className="btn btn-primary">Get Your Own Lost Item Link!</button>
       </form>
     )
   }
@@ -36,11 +43,18 @@ class SignUpForm extends React.Component {
 function SignUp() {
   return (
     <div>
-      <h1>Get Your Own Lost-Item.Com Link</h1>
-      <p>
-        So if you lose your stuff, someone can get it back to you.
-      </p>
-      <SignUpForm />
+      <div className="row">
+        <div className="col-md-6">
+          <h2>Get Your Own Lost-Item.Com Link</h2>
+          <br />
+          <br />
+          <p><strong>Why?</strong> So if you lose your stuff, someone can get it back to you.</p>
+          <p><strong>How much?</strong> Free.</p>
+        </div>
+        <div className="col-md-6">
+          <SignUpForm />
+        </div>
+      </div>
     </div>
   );
 }
