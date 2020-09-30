15c5e6ab967044fe482b6a905855f875093a04ce
Sefhi <echo.mascarell@gmail.com>
2020-09-30 09:06:45 +0200

    Added profile picture to user profiles and pages

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import values from 'object.values';
 import { getFirebaseApp } from './db/FirebaseApp';
@@ -10,12 +11,6 @@ const propTypes = {
 
 const getSlug = () => location.pathname.slice(1);
 
-const MessageSent = () =>
-  <div>
-    <strong>Message Was Sent</strong>
-  </div>;
-
-
 class UserContactForm extends React.Component {
   constructor(props) {
     super(props);
@@ -26,7 +21,7 @@ class UserContactForm extends React.Component {
       slug: getSlug(),
       how: '',
       sending: false,
-      messageSent: false
+      messageSent: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -38,27 +33,27 @@ class UserContactForm extends React.Component {
       sending: true
     })
     fetch('https://us-central1-lost-item-ba357.cloudfunctions.net/notifyUser', {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, cors, *same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-            "Content-Type": "application/json; charset=utf-8",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrer: "no-referrer", // no-referrer, *client
-        body: JSON.stringify(this.state), // body data type must match "Content-Type" header
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, cors, *same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json; charset=utf-8",
+      },
+      redirect: "follow", // manual, *follow, error
+      referrer: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(this.state), // body data type must match "Content-Type" header
     })
-    .then(response => {
-      if (response.status === 200 ){
+      .then(response => {
+        if (response.status === 200) {
+          this.setState({
+            messageSent: true
+          })
+        }
         this.setState({
-           messageSent: true
+          sending: false
         })
-      }
-      this.setState({
-        sending: false
-      })
-    }); // parses response to JSON
+      }); // parses response to JSON
   }
 
   handleChange(name, e) {
@@ -71,65 +66,85 @@ class UserContactForm extends React.Component {
 
     return (
       <div>
-        <form method="POST" onSubmit={this.handleSubmit}>
-          <div className="form-group">
-            <label>What did you find?</label>
-            <input className="form-control" type="text" name="what" onChange={this.handleChange.bind(this, 'what')}/>
-            <small className="form-text text-muted">e.g. an android phone, a credit card, a large sack of potatoes</small>
+        {this.state.messageSent &&
+          <div>
+            <div className="row">
+              <div className="col-md-12">
+                <h2>Thank you!</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-8">
+                <br />
+                <p>🎉</p>
+                <p>{this.props.user.name} will be happy to get his {this.state.what} back!</p>
+                <p>Do you need a lost-item link also? Lost Item gives you a link so if you lose something, the person who finds it can help you get it back.</p>
+                <p className="cta">
+                  <Link className="btn btn-primary" to="/signup">Learn more here.</Link>
+                </p>
+                <hr />
+              </div>
+            </div>
           </div>
-          <div className="form-group">
-            <label>Where did you find it?</label>
-            <input className="form-control" type="text" name="where" onChange={this.handleChange.bind(this, 'where')}/>
-            <small className="form-text text-muted">e.g. at Burger King on Clark Street, at Jenny&apos;s house, on Richard
-              Branson&apos;s jet
-            </small>
-          </div>
-          <div className="form-group">
-            <label>What&#39;s the best way for {this.state.name} to get this item back?</label>
-            <textarea className="form-control" type="text" name="how" onChange={this.handleChange.bind(this, 'how')}/>
-            <small className="form-text text-muted">Leave your contact email or phone here, or a message for
-              how {this.state.name} can retrieve it, like &quot;I left it with the front desk at the ACME Hotel at Colombus and
-              4th Street.&quot;</small>
-          </div>
+        }
+        {!this.state.messageSent &&
+          <div>
+            <div className="row">
+              <div className="col-md-12">
+                <h2>Yay! You found something that belongs to {this.props.user.name}!</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-6">
+                <br />
+                <div className="img" style={{ backgroundImage: `url(${this.props.pic})` }}></div>
+                <br />
+                <p>{this.props.user.name} will be very happy to hear that! Please help get this item returned!</p>
+                <form method="POST" onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <label>What did you find?</label>
+                    <input className="form-control" type="text" name="what" onChange={this.handleChange.bind(this, 'what')} />
+                    <small className="form-text text-muted">e.g. an android phone, a credit card, a large sack of potatoes</small>
+                  </div>
+                  <div className="form-group">
+                    <label>Where did you find it?</label>
+                    <input className="form-control" type="text" name="where" onChange={this.handleChange.bind(this, 'where')} />
+                    <small className="form-text text-muted">e.g. at Burger King on Clark Street, at Jenny&apos;s house, on Richard
+                    Branson&apos;s jet
+                            </small>
+                  </div>
+                  <div className="form-group">
+                    <label>What&#39;s the best way for {this.state.name} to get this item back?</label>
+                    <textarea className="form-control" type="text" name="how" onChange={this.handleChange.bind(this, 'how')} />
+                    <small className="form-text text-muted">Leave your contact email or phone here, or a message for
+                            how {this.state.name} can retrieve it, like &quot;I left it with the front desk at the ACME Hotel at Colombus and
+                            4th Street.&quot;</small>
+                  </div>
 
-          {this.state.messageSent &&
-            <span className="settingsFlashMessage settingsSuccess">Success!</span>
-          }
-          {!this.state.messageSent &&
-            <button type="submit" className="btn btn-primary" disabled={this.state.sending}>
-              <i className={this.state.sending ? 'fa fa-spinner' : 'fa fa-send'} aria-hidden="true"/>
-              &nbsp;&nbsp;{buttonText}</button>
-          }
-        </form>
+                  <button type="submit" className="btn btn-primary" disabled={this.state.sending}>
+                    <i className={this.state.sending ? 'fa fa-spinner' : 'fa fa-send'} aria-hidden="true" />
+                          &nbsp;&nbsp;{buttonText}</button>
+                </form>
+                <hr />
+              </div>
+            </div>
+          </div>
+        }
       </div>
     );
   }
 };
 
 UserContactForm.propTypes = {
-  email: PropTypes.string,
-  name: PropTypes.string,
+  user: PropTypes.object,
+  pic: PropTypes.string,
 };
 
-const UserInfo = (props) =>
-  <div>
-    <div className="row">
-      <div className="col-md-12">
-        <h2>Yay! You found something that belongs to {props.user.name}!</h2>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-md-6">
-        <br />
-        <p>{props.user.name} will be very happy to hear that! Please help get this item returned!</p>
-        <UserContactForm name={props.user.name} email={props.user.email} />
-        <hr />
-      </div>
-    </div>
-  </div>;
+const UserInfo = (props) => <UserContactForm user={props.user} pic={props.pic} />;
 
 UserInfo.propTypes = {
   user: PropTypes.object,
+  pic: PropTypes.string,
 };
 
 
@@ -140,10 +155,18 @@ const UserNotFound = () =>
   </div>;
 
 class UserPage extends React.Component {
-  state = {
-    user: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: null,
+      loading: true,
+      profilePictureURL: 'images/profile.jpg',
+    };
+
+    this.loadImage = this.loadImage.bind(this);
+  }
+
   componentDidMount() {
     const slug = getSlug();
 
@@ -158,9 +181,37 @@ class UserPage extends React.Component {
         } else {
           // do something else;
           const user = values(snapshot.val())[0];
-          this.setState({ user, loading: false });
+          this.loadImage(user);
         }
       });
+  }
+
+  loadImage(user) {
+    try {
+      const storage = getFirebaseApp().storage().ref();
+
+      // After we get the user data, we try to get the profile picture
+      let profilePictureRef = storage.child(`images/${user.email}`);
+
+      // Get user data
+      profilePictureRef.getDownloadURL().then((url) => {
+        // Assign the image to the state
+        this.setState({
+          user,
+          loading: false,
+          profilePictureURL: url,
+        });
+      }).catch(() => {
+        // If no profile picture is found in firebase, we keep the default one set in initial state
+        this.setState({
+          user,
+          loading: false,
+        });
+      });
+    } catch (err) {
+      // We could keep this console.log but I'll comment it for now
+      // console.log('no profile picture, loading default');
+    }
   }
 
   render() {
@@ -170,7 +221,7 @@ class UserPage extends React.Component {
     if (this.state.user === null) {
       return <UserNotFound />;
     }
-    return <UserInfo user={this.state.user} />;
+    return <UserInfo user={this.state.user} pic={this.state.profilePictureURL} />;
   }
 }
 
