06fb98adb681ff9495745a00ad5197506e56197f
Sefhi <echo.mascarell@gmail.com>
2020-10-12 11:30:32 +0200

    new payment page

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -31,7 +31,6 @@ class PayPalCheckoutNR extends React.Component {
     th = this;
   }
 
-
   onAuthorize(data, actions) {
     return actions.payment.execute().then(() => {
       const database = getFirebaseApp().database();
@@ -57,6 +56,25 @@ class PayPalCheckoutNR extends React.Component {
     });
   }
 
+  handleSelectOption = e => {
+    e.stopPropagation();
+
+    // Get both payment options so we can mark one as active
+    let options = document.querySelectorAll('.options .col-sm-6');
+
+    options.forEach(el => {
+      el.classList.remove('active');
+    });
+
+    e.target.classList.add('active');
+
+    amount = e.target.getAttribute('data-value');
+    period = e.target.getAttribute('data-period');
+    this.setState({
+      selectedOption: amount,
+    });
+  }
+
   handleOptionChange = e => {
     amount = e.target.value;
     period = e.target.getAttribute('data-period');
@@ -70,37 +88,66 @@ class PayPalCheckoutNR extends React.Component {
       sandbox: 'AZ_--6AsX9VFy48qw7j4lzza-B2WI7n-iSWr_iRPIFWi0O_AbwNnxYJHjD47x0MMiqLzdg6eEflb3tgh',
       production: 'ASsXllfkIJcAdJkmuqXtqfGNegHSMJee1MKNh5ie4hcreZPMgN2IdhsXq80C6BrThMqUGA1Wn91t_GnT',
     };
+
     return (
       <div>
-        <div>
+        <div className="row payment">
+          <div className="col-md-8 offset-md-2 shadow">
+            <h2 className="big-title color-blue">Change <span>Link</span></h2>
 
-          <p>Get a friendlier, easier to remember lost-item link.</p>
+            <br />
 
-            <p>Instead of lost-item.com/{this.props.location.state.slug}, choose your own custom link! For example, mine is www.lost-item.com/eric - super handy dandy and easy to remember. :)</p>
+            <p className="bigger">Get a friendlier, easier to remember lost-item link.</p>
 
-            <p>We have 2 plans:</p>
+            <p>Instead of <span className="blue">lost-item.com/{this.props.location.state.slug}</span>, choose your own custom link! For example, mine is www.lost-item.com/eric - super handy dandy and easy to remember. :)</p>
 
-            <p>You can hold your custom link for $19.00/year on an annual subscription (You&#39;ll get charged again in a year).</p>
-            <p>For a 1 time payment of $59.00, you get your link forever!</p>
-            <p>Payment is super secure via Paypal.</p>
+            <p className="bigger">What do you want your custom lost-item link to be?</p>
 
-            <p>Which option works best for you?</p>
-          <div>
-            <label htmlFor="1y">
-              <input id="1y" type="radio" data-period="1" value="19.00" name="period" checked={this.state.selectedOption === '19.00'} onChange={this.handleOptionChange} /> $19/year </label>
-          </div>
-          <div>
-            <label htmlFor="forever">
-              <input id="forever" type="radio" data-period="99" value="59.00" name="period" checked={this.state.selectedOption === '59.00'} onChange={this.handleOptionChange} /> $59/forever </label>
+            <div className="divider"></div>
+
+            <p className="bigger center blue">We have 2 plans:</p>
+
+            <p className="bigger center">You can hold your custom link for $19.00/year or an annual subscription.</p>
+
+            <div className="row options">
+              <div className="col-sm-6" id="1y" data-period="1" data-value="19.00" onClick={this.handleSelectOption}>
+                <div className="content">
+                  <h3>$19</h3>
+                  <p>per year</p>
+                </div>
+                <p className="last">(You'll get charged again in a year)</p>
+              </div>
+
+              <div className="col-sm-6" id="forever" data-period="99" data-value="59.00" onClick={this.handleSelectOption}>
+                <div className="content">
+                  <h3>$59</h3>
+                  <p>lifetime</p>
+                </div>
+                <p className="last">(For a 1 time payment of $59.00 you get your link forever!)</p>
+              </div>
+            </div>
+
+            {/* No need to remove the old inputs, we can just hide them just in case */}
+            <div style={{ display: 'none' }}>
+              <div>
+                <label htmlFor="1y">
+                  <input id="1y" type="radio" data-period="1" value="19.00" name="period" checked={this.state.selectedOption === '19.00'} onChange={this.handleOptionChange} /> $19/year </label>
+              </div>
+              <div>
+                <label htmlFor="forever">
+                  <input id="forever" type="radio" data-period="99" value="59.00" name="period" checked={this.state.selectedOption === '59.00'} onChange={this.handleOptionChange} /> $59/forever </label>
+              </div>
+            </div>
+
+            <PayPalButton
+              env={'production'}
+              client={client}
+              payment={this.payment}
+              commit // Optional: show a 'Pay Now' button in the checkout flow
+              onAuthorize={this.onAuthorize}
+            />
           </div>
         </div>
-        <PayPalButton
-          env={'production'}
-          client={client}
-          payment={this.payment}
-          commit // Optional: show a 'Pay Now' button in the checkout flow
-          onAuthorize={this.onAuthorize}
-        />
       </div>
     );
   }
